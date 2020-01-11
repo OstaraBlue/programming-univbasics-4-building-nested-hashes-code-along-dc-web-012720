71eb752af58c6d0af8ccc4bcba429486ff70bfe9
Sonata Arrington <sarrington1009@gmail.com>
2020-01-11 04:36:01 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,7 @@ def bonus
   }
 
   #code your solution here:
-   epic_tragedy[:capulet][:heroine][:status] = "dead"
+   if epic_tragedy[:capulet][:heroine][:status]
    epic_tragedy[:capulet][:hero][:status] = "dead"
   
 
